26f63104c3bde8fdd605d33c784afb663a4432af
GammaRayTea <162896793+GammaRayTea@users.noreply.github.com>
2026-01-31 15:20:20 +0100

    added resource manager, changed maskData to resource

--- game/maskData/maskResourceManager.gd
+++ game/maskData/maskResourceManager.gd
@@ -1,13 +1,25 @@
 extends Node
 
+var masks: Dictionary
+
+
+#func _ready():
+	#var mask = MaskData.new()
+	#print("a")
+	#mask.eyes = MaskComponent.EMOTIONS.SAD
+	#assemble_mask(mask)
 
 
 
-func assemble_mask(data:MaskData) -> Array:
-	return []
-	pass
 
-
-func save_mask():
-	pass
+func assemble_mask(mask:MaskData) -> int:
+	var id = ResourceUID.create_id()
+	masks.set(id,mask)
+	return id
 	
+
+
+
+
+func retrieve_mask(id:int) -> MaskData:
+	return masks.get(id)
