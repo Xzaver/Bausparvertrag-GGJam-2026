8d85037e7b69c004ce7a301da1dd0609eb8992d0
Xaver Schwer <xschwer@proton.me>
2026-01-31 22:12:48 +0100

    update constrctor

--- game/maskContructor/maskConstructor.gd
+++ game/maskContructor/maskConstructor.gd
@@ -5,30 +5,36 @@ class_name MaskConstructor
 
 var currentMaskData :MaskData
 
-@export var eyePos :Node3D
-@export var mouthPos :Node3D
-@export var topPos :Node3D
-@export var shapePos :Node3D
+@export var eyePosRef :Node3D
+@export var mouthPosRef :Node3D
+@export var topPosRef :Node3D
+@export var shapePosRef :Node3D
 
 # --- components
 
-@export var 
-@export var 
-@export var 
-@export var 
-@export var 
-@export var 
-@export var 
-@export var 
-@export var 
-@export var 
-@export var 
-@export var
-@export var 
+@export var eye_SAD :PackedScene 
+@export var eye_HAPPY :PackedScene 
+@export var eye_ANGRY :PackedScene 
+@export var eye_CUTE :PackedScene 
 
+@export var mouth_SAD :PackedScene 
+@export var mouth_HAPPY :PackedScene 
+@export var mouth_ANGRY :PackedScene 
+@export var mouth_CUTE :PackedScene 
 
+@export var top_SAD :PackedScene 
+@export var top_HAPPY :PackedScene 
+@export var top_ANGRY :PackedScene 
+@export var top_CUTE :PackedScene 
 
+@export var shape_SAD :PackedScene 
 
+#--- Mask Pos
+
+var eyePos :Transform3D = eyePosRef.transform
+var mouthPos :Transform3D = mouthPosRef.transform
+var topPos :Transform3D = topPosRef.transform
+var shapePos :Transform3D = shapePosRef.transform
 
 
 signal FinalizeMask(maskDataToSend:MaskData)
