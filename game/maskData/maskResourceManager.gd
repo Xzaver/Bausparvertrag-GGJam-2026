025dceff05f3a3a1c8a8096d5e2b4ab23738f600
GammaRayTea <162896793+GammaRayTea@users.noreply.github.com>
2026-01-31 15:52:17 +0100

    renamed maskResManager funcs

--- game/maskData/maskResourceManager.gd
+++ game/maskData/maskResourceManager.gd
@@ -12,14 +12,16 @@ var masks: Dictionary
 
 
 
-func assemble_mask(mask:MaskData) -> int:
+func save_mask(mask:MaskData) -> int:
 	var id = ResourceUID.create_id()
 	masks.set(id,mask)
 	return id
 	
 
+func set_mask(id:int,mask:MaskData):
+	if masks.has(id):
+		masks.set(id, mask)
 
 
-
-func retrieve_mask(id:int) -> MaskData:
+func fetch_mask(id:int) -> MaskData:
 	return masks.get(id)
