905bf089fd0ad53815ecbcf27b18265901fb9540
Xaver Schwer <xschwer@proton.me>
2026-01-31 12:19:04 +0100

    create Enum categories

--- assets/maskComponents/MaskComponent.gd
+++ assets/maskComponents/MaskComponent.gd
@@ -2,7 +2,20 @@ extends Node
 
 class_name MaskComponent
 
+enum SLOT {
+	EYES = 1,
+	MOUTH = 2,
+	TOP = 3,
+	SHAPE = 4,}
+@export var slot:SLOT
 
+enum EMOTIONS {
+	SAD = 1,
+	HAPPY = 2,
+	ANGRY = 3,
+	CUTE = 4,
+}
+@export var emotions:EMOTIONS
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
