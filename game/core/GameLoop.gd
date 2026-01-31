9ee88d63dd97bc8bf25ce1201b2deeea13fd15ce
Robert D <robert.d@couchinthewoods.de>
2026-01-31 22:11:30 +0100

    added comments

--- game/core/GameLoop.gd
+++ game/core/GameLoop.gd
@@ -4,22 +4,31 @@ var session : GameSession
 var scheduler : Scheduler
 @export var maskBuilder : MaskConstructor
 
+var debugEnabled : bool = true
+
 func _ready() -> void:
 	print("---STARTING GAME LOOP---")
+	
+	#Here you can select difficulty to get difficulty properties. Make difficulty settings in GameSessionProperties class
 	var sessionProperties : GameSessionProperties = GameSessionProperties.GetDifficultyProperties(GameSessionProperties.Difficulty.MEDIUM)
 	session = GameSession.new(sessionProperties)
 	
+	#if you want to access any current session states, like currentCustomer or difficulty settings --> Grab the session object from this GameLoop
+	
+	#This scheduler simply returns a random customer if you call next()
 	scheduler = RandomScheduler.new(session.customers)
 	
 	maskBuilder.FinalizeMask.connect(onMaskFinalized)
 	session.sessionStarted.connect(onSessionStarted)
 	session.stateChanged.connect(onSessionStateChanged)
 	session.Start()
-
+	
+#Just for debug
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("DebugNextState"):
 		session.NextState()
 
+#In Case of initialization stuff
 func onSessionStarted() -> void:
 	print("Session started")
 	
@@ -27,6 +36,7 @@ func onSessionStateChanged(newState : GameSession.GameState) -> void:
 	print("Session changed State: " + GameSession.GameState.keys()[newState])
 	
 	match newState:
+		
 		GameSession.GameState.WaitingForCustomer:
 			
 			session.SetCurrentCustomer(scheduler.Next())
@@ -39,6 +49,10 @@ func onSessionStateChanged(newState : GameSession.GameState) -> void:
 			else:
 				print("Customer has no mask yet. DO IT!")
 			
+			#if debug enabled the program does not automatically go to next state
+			if !debugEnabled:
+				session.SetGameState(GameSession.GameState.ProcessCustomer)
+			
 		GameSession.GameState.ProcessCustomer:
 			
 			var mask : MaskData = MaskResourceManager.fetch_mask(session.currentCustomer.maskID)
@@ -48,26 +62,24 @@ func onSessionStateChanged(newState : GameSession.GameState) -> void:
 			
 			maskBuilder.ReceiveMask(mask)
 			
-			var rndMask : MaskData = MaskData.RandomMask()
+			#if debug enabled we automatically create random mask and go to next state
+			if debugEnabled:
+				var rndMask : MaskData = MaskData.RandomMask()
+				
+				maskBuilder.updateMaskProperty(MaskComponent.SLOT.EYES, rndMask.eyes)
+				maskBuilder.updateMaskProperty(MaskComponent.SLOT.MOUTH, rndMask.mouth)
+				maskBuilder.updateMaskProperty(MaskComponent.SLOT.TOP, rndMask.top)
+				maskBuilder.updateMaskProperty(MaskComponent.SLOT.SHAPE, rndMask.shape)
+				
+				maskBuilder.FinalizeMask.emit(maskBuilder.currentMaskData)
 			
-			maskBuilder.updateMaskProperty(MaskComponent.SLOT.EYES, rndMask.eyes)
-			maskBuilder.updateMaskProperty(MaskComponent.SLOT.MOUTH, rndMask.mouth)
-			maskBuilder.updateMaskProperty(MaskComponent.SLOT.TOP, rndMask.top)
-			maskBuilder.updateMaskProperty(MaskComponent.SLOT.SHAPE, rndMask.shape)
-			
-			maskBuilder.FinalizeMask.emit(maskBuilder.currentMaskData)
-			
-		GameSession.GameState.FinalizeCustomer:
-			#Receive Mask from MaskBuilder
-			#Evaluate Mask Errors
-			#Next State
 			pass
 
 func onMaskFinalized(maskData: MaskData) -> void:
-	session.SetGameState(GameSession.GameState.FinalizeCustomer)
 	
-	if session.currentCustomer.maskID == CustomerData.EMPTY_MASK_ID:
-		session.currentCustomer.maskID = MaskResourceManager.save_mask(maskData)
+	#here user feedback goes
+	session.currentCustomer.maskID = MaskResourceManager.save_mask(maskData)
+	session.SetGameState(GameSession.GameState.FinalizeCustomer)
 		
 	print("Mask finalized: \n" + maskData.ToString())
 	pass
