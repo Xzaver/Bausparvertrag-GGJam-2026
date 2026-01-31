4afdaa8d1ee026dd4dceaa59d8859d2bd0379967
Robert D <robert.d@couchinthewoods.de>
2026-01-31 22:02:47 +0100

    fix Xaver things

--- game/maskContructor/maskConstructor.gd
+++ game/maskContructor/maskConstructor.gd
@@ -24,10 +24,10 @@ func clearBuilder():
 func renderMask(maskData:MaskData):
 	print("Render Mask ")
 	
-	match mas
+	#match mas
 	
-	match maskData:
-		maskData.eyes
+	#match maskData:
+		#maskData.eyes
 	
 	pass
 
