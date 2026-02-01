2df9e5f97cd0e5ecbf1e45ef5c63909919050081
GammaRayTea <162896793+GammaRayTea@users.noreply.github.com>
2026-02-01 11:50:12 +0100

    updated customer data and factory to support cult and sprite types

--- game/core/CustomerData.gd
+++ game/core/CustomerData.gd
@@ -5,6 +5,12 @@ static var EMPTY_MASK_ID = -1
 var id : int
 var maskID : int
 
-func _init(initial_id : int, initialMaskID : int):
+var cult : int
+var sprite : int
+
+func _init(initial_id : int, initialMaskID : int,  initial_sprite : int ,initial_cult : int):
 	id = initial_id
 	maskID = initialMaskID
+	sprite = initial_sprite
+	cult = initial_cult
+	
--- game/core/CustomerFactory.gd
+++ game/core/CustomerFactory.gd
@@ -1,16 +1,26 @@
 class_name CustomerFactory
 
-static func CreateCustomer(id : int) -> CustomerData:
-	var customer : CustomerData = CustomerData.new(id, CustomerData.EMPTY_MASK_ID)
+enum CULT {ELECTRO, SHOE , ANIMAL, PLANT}
+
+
+
+static func CreateCustomer(id : int, sprite : int, cult : int) -> CustomerData:
+	var customer : CustomerData = CustomerData.new(id, CustomerData.EMPTY_MASK_ID, sprite,  cult)
 	print("Created Customer: " + str(customer.id) + " with mask id: " + str(customer.maskID))
 	return customer
 	
 static func CreateCustomers(startingId: int, customerCount: int) -> Array[CustomerData]:
+	
+	
 	var customers : Array[CustomerData] = []
 	var currentID : int = 0
 	
 	for i in range(customerCount):
-		customers.append(CreateCustomer(currentID))
+		var sprite = currentID % 3
+		var cult = currentID % 4
+		print("sprite ",sprite, "cult ", cult)
+		
+		customers.append(CreateCustomer(currentID, sprite, cult))
 		currentID += 1
 		
 	return customers
