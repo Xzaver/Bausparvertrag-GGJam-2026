b764f34b2a0e1d65f4989eb9230176fd7bb270a2
GammaRayTea <162896793+GammaRayTea@users.noreply.github.com>
2026-02-01 15:40:41 +0100

    added textgen

--- game/challenges/TextGenerator.gd
+++ game/challenges/TextGenerator.gd
@@ -1,7 +1,10 @@
 class_name TextGenerator extends Node
 
 @export var label: Label
-
-
+@export var dialogue: JSON
+func _ready() -> void:
+	print(dialogue.data)
+	
+	
 func onCustomerEntered(customer : CustomerData) -> void:
-	pass
+	print(dialogue.data)
